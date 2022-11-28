b2b08c20ff184470ef4045d1f8f9f98af288314a
wamas <wamas@LUNANIA>
2022-11-28 10:15:59 +0100

    This is a test commit

---
+++ GithubTestGR2/Models/TestClass.cs
@@ -0,0 +1,21 @@
+﻿namespace GithubTestGR2.Models
+{
+    public class TestClass
+    {
+        // test
+
+        public int MyProperty { get; set; }
+
+        public int MyPropertyTheSequel { get; set; }
+
+        public bool hasCheese { get; set; }
+
+        public string WittyComment { get; set; }
+
+        public TestClass()
+        {
+            
+        }
+
+    }
+}
